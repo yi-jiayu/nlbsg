f1f182b121aafe5696fe990197daae63ad42580a
Jiayu Yi <yi-jiayu@users.noreply.github.com>
2019-04-01 20:32:12 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='nlblg',
+      version='0.1.0',
+      description='Python SDK for the NLB Open Web Services',
+      url='https://github.com/yi-jiayu/nlbsg',
+      author='Jiayu Yi',
+      author_email='yijiayu@gmail.com',
+      license='MIT',
+      packages=['nlbsg'],
+      zip_safe=False)
