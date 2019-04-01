36db1de8f54a3d7333d913ef56f659343353902d
Jiayu Yi <yi-jiayu@users.noreply.github.com>
2019-04-01 20:35:13 +0800

    setup.py: Add zeep to install_requires

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup
 
 setup(name='nlblg',
-      version='0.1.0',
+      version='0.1.1',
       description='Python SDK for the NLB Open Web Services',
       url='https://github.com/yi-jiayu/nlbsg',
       author='Jiayu Yi',
       author_email='yijiayu@gmail.com',
       license='MIT',
       packages=['nlbsg'],
+      install_requires=[
+          'zeep',
+      ],
       zip_safe=False)
