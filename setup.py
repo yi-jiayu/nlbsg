49fc7ba6efb42d130ab750e88659964c84f8533c
Jiayu Yi <yijiayu@gmail.com>
2019-04-03 12:52:56 +0800

    setup.py: Add classifiers and sort arguments

--- setup.py
+++ setup.py
@@ -10,20 +10,23 @@ with open("nlbsg/__version__.py") as f:
 setup(name='nlbsg',
       version=version['__version__'],
       description='Python SDK for the NLB Open Web Services',
-      long_description=long_description,
       long_description_content_type='text/markdown',
-
+      long_description=long_description,
       url='https://github.com/yi-jiayu/nlbsg',
+      author='Jiayu Yi',
+      author_email='yijiayu@gmail.com',
+      license='MIT',
+      classifiers=[
+          "Programming Language :: Python :: 3.7",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+      ],
       project_urls={
           'Documentation': 'https://nlbsg.readthedocs.io/en/latest/',
           'Source code': 'https://github.com/yi-jiayu/nlbsg',
           'Builds': 'https://travis-ci.com/yi-jiayu/nlbsg',
           'Coverage': 'https://codecov.io/gh/yi-jiayu/nlbsg',
       },
-
-      author='Jiayu Yi',
-      author_email='yijiayu@gmail.com',
-      license='MIT',
       packages=['nlbsg'],
       install_requires=[
           'zeep',
